dd97d00a7acea54003651b89a40a6385820dcaff
Dennis Stevens <cnsdws@yahoo.com>
2015-02-03 19:43:23 -0700

    tab javascript - dir change

--- app/views/_firestringmaster.blade.php
+++ app/views/_firestringmaster.blade.php
@@ -8,7 +8,7 @@
 
     <link href="../css/bootstrap.css" rel="stylesheet">
     <script type="text/javascript" src="http://ajax.googleapis.com/ajax/libs/jquery/1.4.0/jquery.min.js"></script>
-    <script type="text/javascript" src="http://shotplot.net/js/script.js"></script>
+    <script type="text/javascript" src="../../js/script.js"></script>
 
     <!-- Bootstrap -->
     
@@ -87,6 +87,6 @@
   <!-- jQuery (necessary for Bootstrap's JavaScript plugins) -->
     <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.1/jquery.js"></script>
     <!-- Include all compiled plugins (below), or include individual files as needed -->
-    <script src="js/bootstrap.js"></script>
+    <script src="../../js/bootstrap.js"></script>
   </body>
 </html>
